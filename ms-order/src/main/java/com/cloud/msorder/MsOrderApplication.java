43bf8809a8304354e7ee1b119028c91d76dfed09
nick <sjliwei2002@126.com>
2020-09-02 17:07:08 +0800

    upload 6 ms from javahellworld/springcloud-2

---
+++ ms-order/src/main/java/com/cloud/msorder/MsOrderApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.msorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsOrderApplication.class, args);
+    }
+}
